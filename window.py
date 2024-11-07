5e845d6439c1d31378046bb758e7269c20ceefac
pouyannc <pouyannc@gmail.com>
2024-11-07 15:05:47 -0500

    window class

---
+++ window.py
@@ -0,0 +1,24 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.C = Canvas(self.__root, height=height, width=width)
+        self.C.pack()
+        self.running = False
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+    
+    def wait_for_close(self):
+        self.running = True
+        while self.running:
+            self.redraw()
+    
+    def close(self):
+        self.running = False
+
+        
