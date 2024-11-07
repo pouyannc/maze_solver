eb99143b1c2fcdb50cd8523028179d3a0d491f7a
pouyannc <pouyannc@gmail.com>
2024-11-07 16:40:06 -0500

    draw move method to draw a line between cells

--- cell.py
+++ cell.py
@@ -28,3 +28,16 @@ class Cell:
             self._win.draw_line(Line(ptl, ptr), fill_color)
         if self.has_bottom_wall:
             self._win.draw_line(Line(pbl, pbr), fill_color)
+    
+    def draw_move(self, to_cell, undo=False):
+        p1x = (self._x1 + self._x2) / 2
+        p1y = (self._y1 + self._y2) / 2
+        p2x = (to_cell._x1 + to_cell._x2) / 2
+        p2y = (to_cell._y1 + to_cell._y2) / 2
+
+        if undo:
+            fill_color = "red"
+        else:
+            fill_color = "gray"
+
+        self._win.draw_line(Line(Point(p1x, p1y), Point(p2x, p2y)), fill_color)
